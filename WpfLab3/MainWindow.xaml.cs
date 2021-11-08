e6904ad3149f95b045519527a0dffea2ba8eb21d
Sergei Kanter <sdkanter@outlook.com>
2021-11-08 20:43:53 +0100

    done.

--- WpfLab3/MainWindow.xaml.cs
+++ WpfLab3/MainWindow.xaml.cs
@@ -44,47 +44,6 @@ namespace WpfLab3
                 textBox.FontSize = fontSize;
             }
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (textBox.FontWeight == FontWeights.Normal)
-            {
-                textBox.FontWeight = FontWeights.Bold;
-            }
-            else
-            {
-                textBox.FontWeight = FontWeights.Normal;
-            }
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            if (textBox.FontStyle == FontStyles.Italic)
-            {
-                textBox.FontStyle = FontStyles.Normal;
-
-            }
-            else
-            {
-                textBox.FontStyle = FontStyles.Italic;
-
-            }
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            if (textBox.TextDecorations == TextDecorations.Underline)
-            {
-                textBox.TextDecorations = null;
-
-            }
-            else
-            {
-                textBox.TextDecorations = TextDecorations.Underline;
-
-            }
-        }
-
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             if (textBox != null)
@@ -107,7 +66,12 @@ namespace WpfLab3
             }
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Текстовые файлы (*.txt) | *.txt|Все файлы (*.*)|*.*";
@@ -117,19 +81,54 @@ namespace WpfLab3
             }
         }
 
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter= "Текстовые файлы (*.txt) | *.txt|Все файлы (*.*)|*.*";
+            saveFileDialog.Filter = "Текстовые файлы (*.txt) | *.txt|Все файлы (*.*)|*.*";
             if (saveFileDialog.ShowDialog() == true)
             {
-                File.WriteAllText(saveFileDialog.FileName,textBox.Text);
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
             }
         }
 
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        private void BoldExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+        }
+
+        private void ItalicExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Italic)
+            {
+                textBox.FontStyle = FontStyles.Normal;
+
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Italic;
+
+            }
+        }
+
+        private void UnderlineExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == TextDecorations.Underline)
+            {
+                textBox.TextDecorations = null;
+
+            }
+            else
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+
+            }
         }
     }
 }
---
+++ WpfLab3/MyCommands.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfLab3
+{
+    class MyCommands
+    {
+        public static RoutedUICommand Exit { get; set; }
+        public static RoutedCommand Bold { get; set; }
+        public static RoutedCommand Italic { get; set; }
+        public static RoutedCommand Underline { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection boldInput = new InputGestureCollection();
+            boldInput.Add(new KeyGesture(Key.B, ModifierKeys.Control));
+            InputGestureCollection italicInput = new InputGestureCollection();
+            italicInput.Add(new KeyGesture(Key.I, ModifierKeys.Control));
+            InputGestureCollection underlineInput = new InputGestureCollection();
+            underlineInput.Add(new KeyGesture(Key.U, ModifierKeys.Control));
+            Exit = new RoutedUICommand("Выход","Exit",typeof(MyCommands));
+            Bold = new RoutedCommand("Bold", typeof(MyCommands), boldInput);
+            Italic = new RoutedCommand("Italic", typeof(MyCommands), italicInput);
+            Underline = new RoutedCommand("Underline", typeof(MyCommands), underlineInput);
+        }
+    }
+}
